2c57662cc1069bf1ba5eb791385e18c4dd33dac9
Rizarre <seanrizarre.reyes@gmail.com>
2025-07-16 23:18:40 +0800

    Update base path configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  base: process.env.NEXT_PUBLIC_BASE_PATH || "/resume-site",
 };
 
 export default nextConfig;
